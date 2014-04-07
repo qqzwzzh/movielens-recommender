1c1fbb6fe9c4ab8feceefdeae5cd86f1a90feaf0
Santosh Tadikonda <tsantoo@gmail.com>
2014-04-07 02:48:43 -0400

    Added Iteration Code. Pending: Compute correct intiial value. Compute RMSE at each iteration and repeat until RMSE difference is less than tolerated level.

--- src/main/java/UVDriver.java
+++ src/main/java/UVDriver.java
@@ -1,12 +1,18 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -58,14 +64,8 @@ public class UVDriver extends Configured implements Tool {
 
 			// Split each line using seperator based on the dataset.
 			String line[] = null;
-			String seperator = null;
-			if (Settings.BIG_DATA) {
-				seperator = Constants.BIG_DATA_SEPERATOR;
-			} else {
-				seperator = Constants.SMALL_DATA_SEPERATOR;
-			}
-
-			line = value.toString().split(seperator);
+			
+			line = value.toString().split(Settings.INPUT_SEPERATOR);
 
 			keyText.set(line[0]);
 			valText.set(line[1] + "," + line[2]);
@@ -118,11 +118,13 @@ public class UVDriver extends Configured implements Tool {
 	public static class UpdateUMapper extends MapReduceBase implements
 			Mapper<LongWritable, Text, Text, Text> {
 
-		public void map(LongWritable arg0, Text arg1,
-				OutputCollector<Text, Text> arg2, Reporter arg3)
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
-			// TODO Auto-generated method stub
 
+			// Input: (M/U userid, movieid/featureid, rating)
+			String[] line = value.toString().split("\\s");
+			output.collect(new Text(line[1]), value);
 		}
 
 	}
@@ -130,10 +132,158 @@ public class UVDriver extends Configured implements Tool {
 	public static class UpdateUReducer extends MapReduceBase implements
 			Reducer<Text, Text, Text, Text> {
 
-		public void reduce(Text arg0, Iterator<Text> arg1,
-				OutputCollector<Text, Text> arg2, Reporter arg3)
+		// We will be directly using movieID index. So noOfMovies + 1
+		float[][] vFeature = new float[Settings.noOfMovies + 1][Settings.noOfCommonFeatures];
+
+		public void configure(JobConf job) {
+			System.out.println("Configuring UpdateUReducer");
+
+			String vPath = job.get("vPath");
+			System.out.println("VPATH: " + vPath);
+			loadVMatrix(new Path(vPath));
+
+		}
+
+		private void loadVMatrix(Path cachePath) {
+			System.out.println("Loading V Matrix..");
+
+			try {
+				FileSystem fs = FileSystem.get(new Configuration());
+
+				FileStatus[] files = fs.listStatus(cachePath);
+				for (FileStatus f : files) {
+					// If that is a temp file, ingore it.
+					if (new File(f.getPath().toString()).getName().startsWith(
+							"_"))
+						continue;
+					BufferedReader br = new BufferedReader(
+							new InputStreamReader(fs.open(f.getPath())));
+					String line;
+
+					// Input: (V featureID movieID featureValue)
+					while ((line = br.readLine()) != null) {
+						String[] tokens = line.split("\\s");
+						int movieid = Integer.parseInt(tokens[2]);
+						String featureIndex = tokens[1];
+						int fi = Integer.parseInt(featureIndex);
+						String featureValue = tokens[3];
+						float fv = Float.parseFloat(featureValue);
+
+						// Storing each feature value against movieID and
+						// featureIndex.
+						// movieID starts from 1 and featureIndex starts from 0.
+						// So we store at fi - 1.
+						vFeature[movieid][fi - 1] = fv;
+					}
+
+					br.close();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("SWERR: File error.");
+			}
+		}
+
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
-			// TODO Auto-generated method stub
+
+			// Input: (userid, <list of M/U entries with that userid in format
+			// M/U userid movieid/featureid value>)
+
+			ArrayList<Integer> movieIDs = new ArrayList<Integer>();
+			ArrayList<Float> ratings = new ArrayList<Float>();
+			ArrayList<Float> productUV = new ArrayList<Float>();
+
+			float[] uFeature = new float[10];
+
+			while (values.hasNext()) {
+				String line[] = values.next().toString().split("\\s");
+
+				if (line[0].equals("M")) {
+
+					// Store all Movie IDs and ratings into lists. We will use
+					// them later for computation based on the index.
+
+					movieIDs.add(Integer.parseInt(line[2]));
+					ratings.add(Float.parseFloat(line[3]));
+
+					// Initialize the productUV to 0.
+					productUV.add(0f);
+
+				} else if (line[0].equals("U")) {
+
+					// Store the feature values against their index.
+					// Since the U matrix store FI starting with 1, do -1 here.
+					uFeature[Integer.parseInt(line[2]) - 1] = Float
+							.parseFloat(line[3]);
+
+				} else {
+					System.out.println("SWERR: Invalid record.");
+				}
+			}
+
+			// Compute the new feature values for the current user row in U.
+
+			// Update the productUV
+			// For each user vector (row), we are multiplying with corresponding
+			// vector in V matrix which is the movie vector (column)
+			for (int i = 0; i < movieIDs.size(); i++) {
+				int movieID = movieIDs.get(i);
+				float sum = 0;
+				for (int j = 0; j < Settings.noOfCommonFeatures; j++) {
+					sum += uFeature[j] * vFeature[movieID][j];
+				}
+				productUV.set(i, sum);
+			}
+
+			// Update the feature values
+
+			// Pick a random permutation of feature vector to start the update.
+			ArrayList<Integer> featureIndex = new ArrayList<Integer>(10);
+			for (int i = 0; i < Settings.noOfCommonFeatures; i++)
+				featureIndex.add(i);
+			Collections.shuffle(featureIndex);
+
+			// Start to process features as per the permutation order.
+			for (int featureX : featureIndex) {
+
+				// The actual featureIndex we are processing now.
+				int i = featureIndex.get(featureX);
+
+				// For each movieID
+
+				float innerProduct = 0;
+				float viSquare = 0;
+				for (int j = 0; j < movieIDs.size(); j++) {
+					int movieID = movieIDs.get(j);
+					float subtract = ratings.get(j) - productUV.get(j)
+							+ uFeature[i] * vFeature[movieID][i];
+					innerProduct += subtract * vFeature[movieID][i];
+					viSquare += vFeature[movieID][i] * vFeature[movieID][i];
+				}
+
+				float updatedFeatureValue = (float) innerProduct / viSquare;
+
+				for (int j = 0; j < movieIDs.size(); j++) {
+					int mid = movieIDs.get(j);
+
+					// Subtract old feature contribution and add new
+					// feature contribution.
+					productUV.set(j, productUV.get(j) + vFeature[mid][i]
+							* (updatedFeatureValue - uFeature[i]));
+				}
+
+				uFeature[i] = updatedFeatureValue;
+			}
+
+			// Output: (U userId featureIndex featureValue)
+
+			for (int i = 0; i < Settings.noOfCommonFeatures; i++) {
+				output.collect(null, new Text("U " + key.toString() + " "
+						+ (i + 1) + " " + uFeature[i]));
+			}
 
 		}
 
@@ -142,11 +292,15 @@ public class UVDriver extends Configured implements Tool {
 	public static class UpdateVMapper extends MapReduceBase implements
 			Mapper<LongWritable, Text, Text, Text> {
 
-		public void map(LongWritable arg0, Text arg1,
-				OutputCollector<Text, Text> arg2, Reporter arg3)
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
-			// TODO Auto-generated method stub
 
+			// Input: (M/V userid/featureid movieid rating)
+			String[] line = value.toString().split("\\s");
+
+			// Output: (movieid, <value>)
+			output.collect(new Text(line[2]), value);
 		}
 
 	}
@@ -154,17 +308,166 @@ public class UVDriver extends Configured implements Tool {
 	public static class UpdateVReducer extends MapReduceBase implements
 			Reducer<Text, Text, Text, Text> {
 
-		public void reduce(Text arg0, Iterator<Text> arg1,
-				OutputCollector<Text, Text> arg2, Reporter arg3)
+		// We will be directly using userID in index. So noOfUsers + 1
+		float[][] uFeature = new float[Settings.noOfUsers + 1][Settings.noOfCommonFeatures];
+
+		public void configure(JobConf job) {
+			System.out.println("Configuring UpdateUReducer");
+
+			String uPath = job.get("uPath");
+			System.out.println("UPATH: " + uPath);
+			loadUMatrix(new Path(uPath));
+
+		}
+
+		private void loadUMatrix(Path cachePath) {
+			System.out.println("Loading V Matrix..");
+
+			try {
+				FileSystem fs = FileSystem.get(new Configuration());
+
+				FileStatus[] files = fs.listStatus(cachePath);
+				for (FileStatus f : files) {
+					// If that is a temp file, ingore it.
+					if (new File(f.getPath().toString()).getName().startsWith(
+							"_"))
+						continue;
+					BufferedReader br = new BufferedReader(
+							new InputStreamReader(fs.open(f.getPath())));
+					String line;
+
+					// Input: (U UserID featureIndex featureValue)
+					while ((line = br.readLine()) != null) {
+						String[] tokens = line.split("\\s");
+						int userid = Integer.parseInt(tokens[1]);
+						String featureIndex = tokens[2];
+						int fi = Integer.parseInt(featureIndex);
+						String featureValue = tokens[3];
+						float fv = Float.parseFloat(featureValue);
+
+						// Storing each feature value against movieID and
+						// featureIndex.
+						// movieID starts from 1 and featureIndex starts from 0.
+						// So we store at fi - 1.
+						uFeature[userid][fi - 1] = fv;
+					}
+
+					br.close();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("SWERR: File error.");
+			}
+		}
+
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter)
 				throws IOException {
-			// TODO Auto-generated method stub
+
+			// Input: (movieID, <list of M/V entries with that movieID in format
+			// M/V userid/featureid movieid value>)
+
+			ArrayList<Integer> userIDs = new ArrayList<Integer>();
+			ArrayList<Float> ratings = new ArrayList<Float>();
+			ArrayList<Float> productUV = new ArrayList<Float>();
+
+			float[] vFeature = new float[10];
+
+			while (values.hasNext()) {
+				String line[] = values.next().toString().split("\\s");
+
+				if (line[0].equals("M")) {
+
+					// Store all Movie IDs and ratings into lists. We will use
+					// them later for computation based on the index.
+
+					userIDs.add(Integer.parseInt(line[1]));
+					ratings.add(Float.parseFloat(line[3]));
+
+					// Initialize the productUV to 0.
+					productUV.add(0f);
+
+				} else if (line[0].equals("V")) {
+
+					// Store the feature values against their index.
+					// Since the U matrix store FI starting with 1, do -1 here.
+					vFeature[Integer.parseInt(line[1]) - 1] = Float
+							.parseFloat(line[3]);
+
+				} else {
+					System.out.println("SWERR: Invalid record.");
+				}
+			}
+
+			// Compute the new feature values for the current movie row in V.
+
+			// Update the productUV
+			// For each movie vector, we are multiplying with corresponding
+			// vector in U matrix which is the user vector
+			for (int i = 0; i < userIDs.size(); i++) {
+				int userID = userIDs.get(i);
+				float sum = 0;
+				for (int j = 0; j < Settings.noOfCommonFeatures; j++) {
+					sum += vFeature[j] * uFeature[userID][j];
+				}
+				productUV.set(i, sum);
+			}
+
+			// Update the feature values
+
+			// Pick a random permutation of feature vector to start the update.
+			ArrayList<Integer> featureIndex = new ArrayList<Integer>(10);
+			for (int i = 0; i < Settings.noOfCommonFeatures; i++)
+				featureIndex.add(i);
+			Collections.shuffle(featureIndex);
+
+			// Start to process features as per the permutation order.
+			for (int featureX : featureIndex) {
+
+				// The actual featureIndex we are processing now.
+				int i = featureIndex.get(featureX);
+
+				// For each userID
+
+				float innerProduct = 0;
+				float ujSquare = 0;
+				for (int j = 0; j < userIDs.size(); j++) {
+					int userID = userIDs.get(j);
+					float subtract = ratings.get(j) - productUV.get(j)
+							+ vFeature[i] * uFeature[userID][i];
+					innerProduct += subtract * uFeature[userID][i];
+					ujSquare += uFeature[userID][i] * uFeature[userID][i];
+				}
+
+				float updatedFeatureValue = (float) innerProduct / ujSquare;
+
+				for (int j = 0; j < userIDs.size(); j++) {
+					int userID = userIDs.get(j);
+
+					// Subtract old feature contribution and add new
+					// feature contribution.
+					productUV.set(j, productUV.get(j) + uFeature[userID][i]
+							* (updatedFeatureValue - vFeature[i]));
+				}
+
+				vFeature[i] = updatedFeatureValue;
+			}
+
+			// Output: (U userId featureIndex featureValue)
+
+			for (int i = 0; i < Settings.noOfCommonFeatures; i++) {
+				output.collect(null,
+						new Text("V " + (i + 1) + " " + key.toString() + " "
+								+ vFeature[i]));
+			}
 
 		}
 
 	}
 
 	public void optimizeUVMatrix(int matrixType, int iteration)
-			throws IOException, InterruptedException {
+			throws IOException, InterruptedException, URISyntaxException {
 		JobConf conf2 = new JobConf(UVDriver.class);
 
 		conf2.setOutputKeyClass(Text.class);
@@ -224,7 +527,6 @@ public class UVDriver extends Configured implements Tool {
 		JobControl jobControl = new JobControl("jobControl");
 		jobControl.addJob(job2);
 		handleRun(jobControl);
-
 	}
 
 	public void initializeUV() {
@@ -296,7 +598,7 @@ public class UVDriver extends Configured implements Tool {
 		public static int noOfMovies = 0;
 
 		public static final int noOfCommonFeatures = 10;
-		public static final int noOfIterationsRequired = 2;
+		public static final int noOfIterationsRequired = 30;
 
 		public static final String NORMALIZE_DATA_PATH = "normalize";
 		public static String INPUT_PATH = "input";
@@ -309,7 +611,7 @@ public class UVDriver extends Configured implements Tool {
 				noOfUsers = Constants.BIG_DATA_USERS;
 				noOfMovies = Constants.BIG_DATA_MOVIES;
 			} else {
-				INPUT_SEPERATOR = Constants.BIG_DATA_SEPERATOR;
+				INPUT_SEPERATOR = Constants.SMALL_DATA_SEPERATOR;
 				noOfUsers = Constants.SMALL_DATA_USERS;
 				noOfMovies = Constants.SMALL_DATA_MOVIES;
 			}
@@ -349,13 +651,18 @@ public class UVDriver extends Configured implements Tool {
 		Thread t = new Thread(runner);
 		t.start();
 
+		int i = 0;
 		while (!control.allFinished()) {
-			System.out.println(new Date().toString() + ": Still running...");
-			System.out.println("Waiting jobs: "
-					+ control.getWaitingJobs().toString());
-			System.out.println("Successful jobs: "
-					+ control.getSuccessfulJobs().toString());
-			Thread.sleep(5000);
+			if (i % 20 == 0) {
+				System.out
+						.println(new Date().toString() + ": Still running...");
+				System.out.println("Waiting jobs: "
+						+ control.getWaitingJobs().toString());
+				System.out.println("Successful jobs: "
+						+ control.getSuccessfulJobs().toString());
+			}
+			Thread.sleep(1000);
+			i++;
 		}
 	}
 
